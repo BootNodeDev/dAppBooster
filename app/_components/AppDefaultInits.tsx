7ae0edca1de520b448989c3013c1390965c2a300
fernandomg <fernando.greco@gmail.com>
2024-03-22 14:20:23 +0100

    refactor: apply lint fixes

--- app/_components/AppDefaultInits.tsx
+++ app/_components/AppDefaultInits.tsx
@@ -1,16 +1,23 @@
-"use client";
+'use client'
 
-import { WagmiProvider } from "wagmi";
-import { wagmiConfig } from "@/app/_lib/wagmi.config";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClientConfig } from "@/app/_lib/queryClient.config";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import ErrorBoundary from "@/app/_components/ErrorBoundary";
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { WagmiProvider } from 'wagmi'
+
+import ErrorBoundary from '@/app/_components/ErrorBoundary'
+import { queryClientConfig } from '@/app/_lib/queryClient.config'
+import { wagmiConfig } from '@/app/_lib/wagmi.config'
 
 export default function AppDefaultInits({
+  aGreatProp,
+  anotherProp,
   children,
+  someOtherProp,
 }: Readonly<{
-  children: React.ReactNode;
+  aGreatProp: string
+  children: React.ReactNode
+  someOtherProp: number
+  anotherProp: boolean
 }>) {
   return (
     <ErrorBoundary>
@@ -21,5 +28,5 @@ export default function AppDefaultInits({
         </QueryClientProvider>
       </WagmiProvider>
     </ErrorBoundary>
-  );
+  )
 }
