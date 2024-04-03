4707abe11ee7d6c3781a1eb4275dce90ae56a7de
luchobonatti <luciano@bootnode.dev>
2024-04-03 18:42:43 -0300

    Fix lint import order

--- app/_components/AppDefaultInits.tsx
+++ app/_components/AppDefaultInits.tsx
@@ -3,8 +3,8 @@
 import { useState } from 'react'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiProvider } from 'wagmi'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { WagmiProvider } from 'wagmi'
 
 import ErrorBoundary from '@/app/_components/ErrorBoundary'
 import { queryClientConfig, wagmiConfig } from '@/app/_lib/wagmi.config'
