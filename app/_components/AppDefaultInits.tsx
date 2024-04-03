dbbc6417ff4efe1131eef4685b15d8760f625e26
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2024-04-03 14:58:54 -0300

    chore: add husky and lint-staged (#52)
    * add: husky + lint-staged
    
    * add: some missing entries in .gitignore
    
    * add: missing linting modules and a few more rules
    
    * remove: unnecessary test code
    
    * add: less severe rule for unused vars
    
    * remove: redundant eslint plugins
    fix: wrong esilnt rule
    
    * fix: vercel build issue

--- app/_components/AppDefaultInits.tsx
+++ app/_components/AppDefaultInits.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import React from 'react'
 
 import { QueryClientProvider } from '@tanstack/react-query'
 import { QueryClient } from '@tanstack/react-query'
