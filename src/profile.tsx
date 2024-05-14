55a1fbddc5098c9928d2c19c70626e4d8ceb0757
fernandomg <fernando.greco@gmail.com>
2024-05-14 19:38:13 +0200

    feat: add wagmi

---
+++ src/profile.tsx
@@ -0,0 +1,10 @@
+import { useAccount, useEnsName } from "wagmi";
+
+export function Profile() {
+  const { address } = useAccount();
+  const { data, error, status } = useEnsName({ address });
+  if (status === "pending") return <div>Loading ENS name</div>;
+  if (status === "error")
+    return <div>Error fetching ENS name: {error.message}</div>;
+  return <div>ENS name: {data}</div>;
+}
