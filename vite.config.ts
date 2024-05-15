e5858c0bca1b9f3ee3b918617fca0093dd254f02
fernandomg <fernando.greco@gmail.com>
2024-05-15 13:32:15 +0200

    fix: build for path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      '@/src': path.resolve(__dirname, './src'),
+    },
+  },
 })
