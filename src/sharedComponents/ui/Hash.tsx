359bccf39692e40ef31bd6b2dd5177c0a9b20701
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-06-11 15:59:16 -0300

    docs: updated a few things in the Hash component's docs

--- src/sharedComponents/ui/Hash.tsx
+++ src/sharedComponents/ui/Hash.tsx
@@ -31,10 +31,10 @@ interface Props {
  * Hash component, displays a hash with an optional copy button and an optional external link.
  *
  * @param {string} hash - The hash to display.
- * @param {string} [explorerURL] - The URL to the explorer for the hash. If provided, an external link icon will be displayed. Default is an empty string.
+ * @param {string} [explorerURL=''] - The URL to the explorer for the hash. If provided, an external link icon will be displayed. Default is an empty string.
  * @param {MouseEventHandler<HTMLButtonElement>} [onCopy=undefined] - The function to call when the copy button is clicked. Default is undefined.
  * @param {boolean} [showCopyButton=false] - Whether to show the copy button. Default is false.
- * @param {number | 'disabled'} [truncatedHashLength] - The number of characters to show at the start and end of the hash. 'disabled' if you don't want to truncate the hash value. Default is 6.
+ * @param {number | 'disabled'} [truncatedHashLength=6] - The number of characters to show at the start and end of the hash. 'disabled' if you don't want to truncate the hash value. Default is 6.
  */
 const Hash: React.FC<Props> = ({
   explorerURL = '',
