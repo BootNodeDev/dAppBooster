9825e71a763e726e128d99126a4fb082ceccf3ee
luchobonatti <luciano@bootnode.dev>
2024-06-11 17:32:41 -0300

    feat: update Avatar component to use dynamic sizing

--- src/sharedComponents/web3/Avatar.tsx
+++ src/sharedComponents/web3/Avatar.tsx
@@ -6,22 +6,24 @@ import CustomAvatar from '@/src/sharedComponents/ui/Avatar'
 
 interface AvatarProps {
   address: Address
+  size?: number
 }
 
 /**
  * Avatar WEB3 component using wagmi hooks to fetch ENS name and avatar image for the provided address.
  *
  * @param {string} props.address - The address
+ * @param {number} [props.size=50] - The size of the avatar
  * @example <Avatar address="0x1234567890abcdef1234567890abcdef12345678" />
  */
-const Avatar = ({ address }: AvatarProps) => {
+const Avatar = ({ address, size = 50 }: AvatarProps) => {
   const { data: ensName } = useEnsName({ address })
 
   const { data: avatarImg } = useEnsAvatar({
     name: ensName ? normalize(ensName) : undefined,
   })
 
-  return <CustomAvatar address={address} ensImage={avatarImg} ensName={ensName} size={50} />
+  return <CustomAvatar address={address} ensImage={avatarImg} ensName={ensName} size={size} />
 }
 
 export default Avatar
