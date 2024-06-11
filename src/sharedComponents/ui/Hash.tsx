2ed48ff950556c7aa891e37aea36122f9f890bc9
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-06-11 15:56:21 -0300

    refactor: add a way to avoid truncating the hash

--- src/sharedComponents/ui/Hash.tsx
+++ src/sharedComponents/ui/Hash.tsx
@@ -9,16 +9,22 @@ const Wrapper = styled.div`
   align-items: center;
   column-gap: 8px;
   display: flex;
+  max-width: 100%;
 `
 
-const HashValue = styled.span``
+const HashValue = styled.span`
+  max-width: fit-content;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
 
 interface Props {
   explorerURL?: string
   hash: string
   onCopy?: MouseEventHandler<HTMLButtonElement>
   showCopyButton?: boolean
-  truncatedLength?: number
+  truncatedHashLength?: number | 'disabled'
 }
 
 /**
@@ -28,19 +34,21 @@ interface Props {
  * @param {string} [explorerURL] - The URL to the explorer for the hash. If provided, an external link icon will be displayed. Default is an empty string.
  * @param {MouseEventHandler<HTMLButtonElement>} [onCopy=undefined] - The function to call when the copy button is clicked. Default is undefined.
  * @param {boolean} [showCopyButton=false] - Whether to show the copy button. Default is false.
- * @param {number} [truncatedLength] - The number of characters to show at the end of the hash. Default is 6.
+ * @param {number | 'disabled'} [truncatedHashLength] - The number of characters to show at the start and end of the hash. 'disabled' if you don't want to truncate the hash value. Default is 6.
  */
 const Hash: React.FC<Props> = ({
   explorerURL = '',
   hash,
   onCopy,
   showCopyButton = false,
-  truncatedLength,
+  truncatedHashLength = 6,
   ...restProps
 }) => {
   return (
     <Wrapper {...restProps}>
-      <HashValue>{getTruncatedHash(hash, truncatedLength)}</HashValue>
+      <HashValue>
+        {truncatedHashLength === 'disabled' ? hash : getTruncatedHash(hash, truncatedHashLength)}
+      </HashValue>
       {showCopyButton && <CopyButton onClick={onCopy} value={hash} />}
       {explorerURL && <ExternalLink href={explorerURL} />}
     </Wrapper>
