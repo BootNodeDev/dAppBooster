f56f80f7354aee0089dde991d64961fb356e5f40
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-06-13 12:02:33 -0300

    refactor: update home

--- src/pageComponents/home/index.tsx
+++ src/pageComponents/home/index.tsx
@@ -1,45 +1,21 @@
 import styled from 'styled-components'
 
-import { Title, Text } from 'db-ui-toolkit'
-import { useEnsName } from 'wagmi'
-import { mainnet } from 'wagmi/chains'
-
-import Hash from '@/src/sharedComponents/ui/Hash'
+import Examples from '@/src/pageComponents/home/Examples'
+import WelcomeSection from '@/src/pageComponents/home/WelcomeSection'
 
 const Wrapper = styled.div`
   display: flex;
-  flex-grow: 1;
-  justify-content: center;
   flex-direction: column;
-  align-items: center;
+  flex-grow: 1;
+  margin-top: calc(var(--base-header-height) * -1);
+  position: relative;
 `
 
-export const Home = () => {
-  const { data, error, status } = useEnsName({
-    address: '0x87885AaEEdED51C7e3858a782644F5d89759f245',
-    chainId: mainnet.id,
-  })
-
+export const Home = ({ ...restProps }) => {
   return (
-    <Wrapper>
-      <Title>Welcome to dApp👻ster!</Title>
-      <Text>
-        {status === 'pending' ? (
-          <>Loading ENS name</>
-        ) : status === 'error' ? (
-          <>Error fetching ENS name: {error.message}</>
-        ) : (
-          <>
-            <b>ENS name:</b> {data}
-          </>
-        )}
-      </Text>
-      <Hash
-        explorerURL="https://etherscan.io/address/0x87885aaeeded51c7e3858a782644f5d89759f245"
-        hash="0x87885aaeeded51c7e3858a782644f5d89759f245"
-        onCopy={() => console.log('Copied!')}
-        showCopyButton
-      />
+    <Wrapper {...restProps}>
+      <WelcomeSection />
+      <Examples />
     </Wrapper>
   )
 }
