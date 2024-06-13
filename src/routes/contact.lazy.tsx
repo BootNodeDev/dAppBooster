3981df2cc96941d778ab4d670a28f9ece574ba42
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-06-13 00:52:55 -0300

    chore: remove some unused components

--- src/routes/contact.lazy.tsx
+++
@@ -1,7 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
-
-import { Contact } from '@/src/pageComponents/contact'
-
-export const Route = createLazyFileRoute('/contact')({
-  component: Contact,
-})
