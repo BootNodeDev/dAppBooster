7fd7aba56646a58b228bbfe513f14d458adab0f8
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-06-13 12:00:14 -0300

    feat: add basic examples component

---
+++ src/pageComponents/home/Examples/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { Text, InnerContainer as Inner, ContainerPadding } from 'db-ui-toolkit'
+import { useEnsName } from 'wagmi'
+import { mainnet } from 'wagmi/chains'
+
+import Hash from '@/src/sharedComponents/ui/Hash'
+
+const Wrapper = styled.section`
+  background-color: var(--landing-page-main-background-color);
+  flex-grow: 1;
+`
+
+const InnerContainer = styled(Inner)`
+  flex-direction: column;
+
+  ${ContainerPadding}
+`
+
+const Examples: React.FC = ({ ...restProps }) => {
+  const { data, error, status } = useEnsName({
+    address: '0x87885AaEEdED51C7e3858a782644F5d89759f245',
+    chainId: mainnet.id,
+  })
+
+  return (
+    <Wrapper id="examples" {...restProps}>
+      <InnerContainer>
+        <Text>
+          {status === 'pending' ? (
+            <>Loading ENS name</>
+          ) : status === 'error' ? (
+            <>Error fetching ENS name: {error.message}</>
+          ) : (
+            <>
+              <b>ENS name:</b> {data}
+            </>
+          )}
+        </Text>
+        <Hash
+          explorerURL="https://etherscan.io/address/0x87885aaeeded51c7e3858a782644f5d89759f245"
+          hash="0x87885aaeeded51c7e3858a782644f5d89759f245"
+          onCopy={() => console.log('Copied!')}
+          showCopyButton
+        />
+      </InnerContainer>
+    </Wrapper>
+  )
+}
+
+export default Examples
