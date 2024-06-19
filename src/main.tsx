6c440b70e1050073f5dadfb155f4e9ccf36d9b45
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-06-19 15:22:36 -0300

    refactor: add DOMContentLoaded

--- src/main.tsx
+++ src/main.tsx
@@ -15,73 +15,77 @@ declare module '@tanstack/react-router' {
 
 const rootElement = document.getElementById('root')!
 
-const getSiteURL = () => {
-  const { hostname, port, protocol } =
-    typeof window !== 'undefined'
-      ? window.location
-      : { hostname: 'localhost', port: 5173, protocol: 'http:' }
-  const portString = port ? `:${port}` : ''
-  return typeof window !== 'undefined' ? `${protocol}//${hostname}${portString}` : ''
-}
+const setAppInfo = () => {
+  const getSiteURL = () => {
+    const { hostname, port, protocol } =
+      typeof window !== 'undefined'
+        ? window.location
+        : { hostname: 'localhost', port: 5173, protocol: 'http:' }
+    const portString = port ? `:${port}` : ''
+    return typeof window !== 'undefined' ? `${protocol}//${hostname}${portString}` : ''
+  }
 
-const addMetaTag = (props: { name?: string; property?: string; content: string }) => {
-  const { content, name, property } = props
+  const addMetaTag = (props: { name?: string; property?: string; content: string }) => {
+    const { content, name, property } = props
+
+    /**
+     * You need to provide at least name or property
+     */
+    if (!name && !property) return
+
+    const meta = document.createElement('meta')
+
+    meta.setAttribute('content', content)
+
+    if (property) {
+      meta.setAttribute('property', property)
+    }
+
+    if (name) {
+      meta.setAttribute('name', name)
+    }
+
+    document.head.appendChild(meta)
+  }
 
   /**
-   * You need to provide at least name or property
+   * Get some meta tags from env
    */
-  if (!name && !property) return
+  const siteURL = getSiteURL()
+  const title = import.meta.env.PUBLIC_APP_NAME
+  const description = import.meta.env.PUBLIC_APP_DESCRIPTION || title
+  const twitterHandle = import.meta.env.PUBLIC_APP_TWITTER_HANDLE || ''
+  const keywords = import.meta.env.PUBLIC_APP_KEYWORDS || ''
+  const shareImage = import.meta.env.PUBLIC_APP_SHARE_IMAGE || '/shareable/ogImage.jpg'
 
-  const meta = document.createElement('meta')
+  /**
+   * Mandatory / pre-defined meta tags
+   */
+  const meta = [
+    // Open Graph meta tags
+    { property: 'og:image', content: `${siteURL}${shareImage}` },
+    { property: 'og:title', content: title },
+    { property: 'og:url', content: siteURL },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:description', content: description },
+    // Twitter meta tags
+    { name: 'twitter:image', content: `${siteURL}${shareImage}` },
+    { name: 'twitter:creator', content: twitterHandle },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:site', content: title },
+    // Standard meta tags
+    { name: 'description', content: description },
+    { name: 'keywords', content: keywords },
+  ]
 
-  meta.setAttribute('content', content)
-
-  if (property) {
-    meta.setAttribute('property', property)
-  }
-
-  if (name) {
-    meta.setAttribute('name', name)
-  }
-
-  document.head.appendChild(meta)
+  /**
+   * Update meta tags and title
+   */
+  meta.map((item) => addMetaTag({ ...item }))
+  document.title = title
 }
 
-/**
- * Get some meta tags from env
- */
-const siteURL = getSiteURL()
-const title = import.meta.env.PUBLIC_APP_NAME
-const description = import.meta.env.PUBLIC_APP_DESCRIPTION || title
-const twitterHandle = import.meta.env.PUBLIC_APP_TWITTER_HANDLE || ''
-const keywords = import.meta.env.PUBLIC_APP_KEYWORDS || ''
-const shareImage = import.meta.env.PUBLIC_APP_SHARE_IMAGE || '/shareable/ogImage.jpg'
-
-/**
- * Mandatory / pre-defined meta tags
- */
-const meta = [
-  // Open Graph meta tags
-  { property: 'og:image', content: `${siteURL}${shareImage}` },
-  { property: 'og:title', content: title },
-  { property: 'og:url', content: siteURL },
-  { property: 'og:type', content: 'website' },
-  { property: 'og:description', content: description },
-  // Twitter meta tags
-  { name: 'twitter:image', content: `${siteURL}${shareImage}` },
-  { name: 'twitter:creator', content: twitterHandle },
-  { name: 'twitter:card', content: 'summary_large_image' },
-  { name: 'twitter:site', content: title },
-  // Standard meta tags
-  { name: 'description', content: description },
-  { name: 'keywords', content: keywords },
-]
-
-/**
- * Update meta tags and title
- */
-document.title = title
-meta.map((item) => addMetaTag({ ...item }))
+document.addEventListener('DOMContentLoaded', setAppInfo)
 
 if (!rootElement.innerHTML) {
   ReactDOM.createRoot(rootElement).render(
