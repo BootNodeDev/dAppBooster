453301e097305044e73f970fffc06e632021dcb9
fernandomg <fernando.greco@gmail.com>
2024-06-24 14:02:21 +0200

    feat: add search and list examples

--- src/pageComponents/home/Examples/Items/Tokens.tsx
+++ src/pageComponents/home/Examples/Items/Tokens.tsx
@@ -1,21 +1,25 @@
-import { useRef, useState } from 'react'
+import { FC, useRef, useState } from 'react'
+import styled from 'styled-components'
 
 import { useVirtualizer } from '@tanstack/react-virtual'
-import { mainnet } from 'wagmi/chains'
+import { Title } from 'db-ui-toolkit'
+import { arbitrum, mainnet } from 'viem/chains'
 
+import TokenLogo from '@/src/sharedComponents/TokenLogo'
 import { useTokenSearch, useTokens } from '@/src/sharedComponents/TokensProvider'
+import { type Tokens as TokensList } from '@/src/token'
+import { getTruncatedHash } from '@/src/utils/strings'
 
-const Tokens = () => {
-  const { tokensByChainId } = useTokens()
+const TokensList: FC<{ tokenList: TokensList }> = ({ tokenList }) => {
   const [searchTerm, setSearchTerm] = useState('')
-  const searchResult = useTokenSearch({ searchTerm, tokens: tokensByChainId[mainnet.id] })
+  const searchResult = useTokenSearch({ searchTerm, tokens: tokenList })
 
   const parentRef = useRef(null)
 
   const rowVirtualizer = useVirtualizer({
     count: searchResult.length,
     getScrollElement: () => parentRef.current,
-    estimateSize: () => 35,
+    estimateSize: () => 30,
     overscan: 5,
   })
 
@@ -27,10 +31,12 @@ const Tokens = () => {
       <div
         ref={parentRef}
         style={{
+          marginTop: '10px',
           height: `400px`,
-          width: '80%',
-          overflow: 'auto',
-          outline: '1px solid red',
+          width: '300px',
+          overflowY: 'auto',
+          overflowX: 'hidden',
+          outline: '1px solid #efefef',
         }}
       >
         {/* The large inner element to hold all of the items */}
@@ -49,13 +55,17 @@ const Tokens = () => {
                 position: 'absolute',
                 top: 0,
                 left: 0,
-                width: '100%',
                 height: `${virtualItem.size}px`,
                 transform: `translateY(${virtualItem.start}px)`,
+                display: 'flex',
+                columnGap: '10px',
+                width: '100vw',
               }}
             >
-              {searchResult[virtualItem.index].address} - {searchResult[virtualItem.index].symbol} (
-              {searchResult[virtualItem.index].chainId})
+              <TokenLogo token={searchResult[virtualItem.index]} />
+              {searchResult[virtualItem.index].symbol}
+              {' - '}
+              {getTruncatedHash(searchResult[virtualItem.index].address)}
             </div>
           ))}
         </div>
@@ -64,4 +74,42 @@ const Tokens = () => {
   )
 }
 
-export default Tokens
+export const TokensMainnet = () => {
+  const { tokensByChainId } = useTokens()
+
+  return (
+    <div>
+      <Title>Mainnet</Title>
+      <TokensList tokenList={tokensByChainId[mainnet.id]} />
+    </div>
+  )
+}
+
+export const TokensArbitrum = () => {
+  const { tokensByChainId } = useTokens()
+
+  return (
+    <div>
+      <Title>Arbitrum</Title>
+      <TokensList tokenList={tokensByChainId[arbitrum.id]} />
+    </div>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  direction: row;
+  column-gap: 30px;
+  outline: 1px solid #afafaf;
+  padding: 10px;
+`
+const MultipleTokens = () => {
+  return (
+    <Wrapper>
+      <TokensMainnet />
+      <TokensArbitrum />
+    </Wrapper>
+  )
+}
+
+export default MultipleTokens
