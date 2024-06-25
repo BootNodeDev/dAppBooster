c1afa27cb2b84cc47cc2459c7b0cd0930aebd758
luchobonatti <luciano@bootnode.dev>
2024-06-25 12:40:04 -0300

    feat: add ExplorerLink component

---
+++ src/sharedComponents/ExplorerLink.tsx
@@ -0,0 +1,13 @@
+import { GetExplorerUrlParams, getExplorerLink } from '@/src/utils/getExplorerLink'
+
+interface ExplorerLinkProps extends GetExplorerUrlParams {
+  text?: string
+}
+
+export const ExplorerLink = ({ text = 'View on explorer', ...props }: ExplorerLinkProps) => {
+  return (
+    <a href={getExplorerLink(props)} rel="noopener noreferrer" target="_blank">
+      {text}
+    </a>
+  )
+}
