45fb50d0027cbb44836d4b5071038dfbecd35187
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-06-27 15:58:35 -0300

    feat: add 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import React from 'react'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import ReactDOM from 'react-dom/client'
 
+import NotFound404 from '@/src/pageComponents/NotFound404'
 import { routeTree } from '@/src/routeTree.gen'
 
-const router = createRouter({ routeTree })
+const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: () => <NotFound404 />,
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
