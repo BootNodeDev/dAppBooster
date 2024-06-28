58655764b2b1eb03297f1a89eda9f2873f687430
luchobonatti <luciano@bootnode.dev>
2024-06-28 11:53:55 -0300

    feat: optimize chains configuration for development mode

--- src/lib/networks.config.ts
+++ src/lib/networks.config.ts
@@ -3,7 +3,11 @@ import { mainnet, optimismSepolia, sepolia } from 'viem/chains'
 
 import { env } from '@/src/env'
 
-export const chains = [mainnet, optimismSepolia, sepolia] as const
+const isDevMode = env.PUBLIC_DEV_MODE === 'true'
+
+export const allChains = [mainnet, optimismSepolia, sepolia] as const
+
+export const chains = allChains.filter((chain) => (isDevMode ? chain.testnet : !chain.testnet))
 
 type RestrictedTransports = Record<(typeof chains)[number]['id'], Transport>
 
