e8b3efc514e15a9ecaea9ed1761df10c0ad37c17
luchobonatti <luciano@bootnode.dev>
2024-06-28 11:47:28 -0300

    feat: add dev mode option to environment configuration

--- src/env.ts
+++ src/env.ts
@@ -14,6 +14,7 @@ export const env = createEnv({
     PUBLIC_RPC_OP_SEPOLIA: z.string().optional(),
     PUBLIC_RPC_SEPOLIA: z.string().optional(),
     PUBLIC_WALLETCONNECT_PROJECT_ID: z.string().length(32),
+    PUBLIC_DEV_MODE: z.string().optional(),
   },
   runtimeEnv: import.meta.env,
   emptyStringAsUndefined: true,
