6113382c9a429be3707b549ada9a35bfadd94f12
fernandomg <fernando.greco@gmail.com>
2024-06-28 13:51:03 +0200

    feat(chains): add support for polygon and arbitrum

--- src/lib/networks.config.ts
+++ src/lib/networks.config.ts
@@ -1,14 +1,16 @@
 import { http, type Transport } from 'viem'
-import { mainnet, optimismSepolia, sepolia } from 'viem/chains'
+import { arbitrum, mainnet, optimismSepolia, sepolia, polygon } from 'viem/chains'
 
 import { env } from '@/src/env'
 
-export const chains = [mainnet, optimismSepolia, sepolia] as const
+export const chains = [arbitrum, mainnet, optimismSepolia, polygon, sepolia] as const
 
 type RestrictedTransports = Record<(typeof chains)[number]['id'], Transport>
 
 export const transports: RestrictedTransports = {
+  [arbitrum.id]: http(env.PUBLIC_RPC_ARBITRUM),
   [mainnet.id]: http(env.PUBLIC_RPC_MAINNET),
-  [optimismSepolia.id]: http(env.PUBLIC_RPC_OP_SEPOLIA),
+  [optimismSepolia.id]: http(env.PUBLIC_RPC_OPTIMISM_SEPOLIA),
+  [polygon.id]: http(env.PUBLIC_RPC_POLYGON),
   [sepolia.id]: http(env.PUBLIC_RPC_SEPOLIA),
 }
