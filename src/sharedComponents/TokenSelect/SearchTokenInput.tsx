c396e9bf7088dff64a9b391c543163d41e71d89c
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-07-01 16:35:30 -0300

    refactor: rename component

--- src/sharedComponents/TokenSelect/SearchTokenInput.tsx
+++
@@ -1,7 +0,0 @@
-import { type FC, InputHTMLAttributes } from 'react'
-
-const SearchTokenInput: FC<InputHTMLAttributes<HTMLInputElement>> = ({ ...restProps }) => {
-  return <input type="text" {...restProps} />
-}
-
-export default SearchTokenInput
