daf4d6816d722c5b45099568abac72afee97db4f
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-07-02 17:03:54 -0300

    fix: remove wrong prop

--- src/pageComponents/home/Examples/demos/TokenDropdown/index.tsx
+++ src/pageComponents/home/Examples/demos/TokenDropdown/index.tsx
@@ -69,7 +69,6 @@ const TokenDropdown: React.FC = ({ ...restProps }) => {
             )}
           </DropdownButton>
         }
-        clearDropdownStyle
         items={
           <TokenSelect
             currentNetworkId={currentNetworkId}
