c133d1f813264d5dcf51eddb881c093ad8e90884
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-07-05 18:21:53 -0300

    chore: changed TokenDopdownDemo folder's name to TokenDropdown

--- src/pageComponents/home/Examples/demos/TokenDropdown/index.tsx
+++ src/pageComponents/home/Examples/demos/TokenDropdown/index.tsx
@@ -2,9 +2,9 @@ import { type FC, useState } from 'react'
 
 import { arbitrum, mainnet, polygon } from 'viem/chains'
 
-import Arbitrum from '@/src/pageComponents/home/Examples/demos/TokenDropdownDemo/assets/Arbitrum'
-import Eth from '@/src/pageComponents/home/Examples/demos/TokenDropdownDemo/assets/Eth'
-import Polygon from '@/src/pageComponents/home/Examples/demos/TokenDropdownDemo/assets/Polygon'
+import Arbitrum from '@/src/pageComponents/home/Examples/demos/TokenDropdown/assets/Arbitrum'
+import Eth from '@/src/pageComponents/home/Examples/demos/TokenDropdown/assets/Eth'
+import Polygon from '@/src/pageComponents/home/Examples/demos/TokenDropdown/assets/Polygon'
 import TokenDropdown from '@/src/sharedComponents/TokenDropdown'
 import { type Networks } from '@/src/sharedComponents/TokenSelect'
 import { type Token } from '@/src/types/token'
