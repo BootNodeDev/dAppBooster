9de3f2943d6e5afd863e7ed93a5dd250b52e51c3
fernandomg <fernando.greco@gmail.com>
2024-07-08 12:51:31 +0200

    feat(token list): add the native token to TopTokens list
    Also, included OP as optional network in examples

--- src/pageComponents/home/Examples/demos/TokenDropdownDemo/index.tsx
+++ src/pageComponents/home/Examples/demos/TokenDropdownDemo/index.tsx
@@ -1,9 +1,10 @@
 import { type FC, useState } from 'react'
 
-import { arbitrum, mainnet, polygon } from 'viem/chains'
+import { arbitrum, mainnet, optimism, polygon } from 'viem/chains'
 
 import Arbitrum from '@/src/pageComponents/home/Examples/demos/TokenDropdownDemo/assets/Arbitrum'
 import Eth from '@/src/pageComponents/home/Examples/demos/TokenDropdownDemo/assets/Eth'
+import Optimism from '@/src/pageComponents/home/Examples/demos/TokenDropdownDemo/assets/Optimism'
 import Polygon from '@/src/pageComponents/home/Examples/demos/TokenDropdownDemo/assets/Polygon'
 import TokenDropdown from '@/src/sharedComponents/TokenDropdown'
 import { type Networks } from '@/src/sharedComponents/TokenSelect'
@@ -18,6 +19,12 @@ const TokenDropdownDemo: FC = ({ ...restProps }) => {
       id: mainnet.id,
       label: mainnet.name,
       onClick: () => setCurrentNetworkId(mainnet.id),
+    },
+    {
+      icon: <Optimism />,
+      id: optimism.id,
+      label: optimism.name,
+      onClick: () => setCurrentNetworkId(optimism.id),
     },
     {
       icon: <Arbitrum />,
