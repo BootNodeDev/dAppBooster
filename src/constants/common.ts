e036cb3472b023b1b44714ed8d77b90084c1ef42
fernandomg <fernando.greco@gmail.com>
2024-07-19 14:57:51 +0200

    feat(subgraphs): render conditionally

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -2,3 +2,10 @@ import { env } from '@/src/env'
 
 export const isDev = import.meta.env.DEV
 export const includeTestNets = env.PUBLIC_INCLUDE_TESTNETS === 'true'
+export const isSubgraphConfigValid = Boolean(
+  env.PUBLIC_SUBGRAPHS_API_KEY &&
+    env.PUBLIC_SUBGRAPHS_CHAINS_RESOURCE_IDS &&
+    env.PUBLIC_SUBGRAPHS_DEVELOPMENT_URL &&
+    env.PUBLIC_SUBGRAPHS_PRODUCTION_URL &&
+    env.PUBLIC_SUBGRAPHS_ENVIRONMENT,
+)
---
+++ src/pageComponents/home/Examples/demos/SubgraphLoader.tsx
@@ -0,0 +1,44 @@
+import { lazy, useState } from 'react'
+import styled from 'styled-components'
+
+import { Button } from 'db-ui-toolkit'
+
+import { isSubgraphConfigValid } from '@/src/constants/common'
+import { withSuspenseAndRetry } from '@/src/utils/suspenseWrapper'
+
+const Subgraph = lazy(() => import('./Subgraph'))
+
+const Wrapper = styled.p`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+`
+
+const SubgraphLoader = withSuspenseAndRetry(() => {
+  const [showSubgraph, setShowSubgraph] = useState(false)
+
+  return showSubgraph ? (
+    <Subgraph />
+  ) : (
+    <>
+      <Button disabled={!isSubgraphConfigValid} onClick={() => setShowSubgraph(true)}>
+        Load Subgraph
+      </Button>
+      {!isSubgraphConfigValid && (
+        <Wrapper>
+          Be sure to have properly confiugred the{' '}
+          <a
+            href="https://github.com/bootnodedev/dAppBooster#subgraphs"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Subgraph env variables
+          </a>
+        </Wrapper>
+      )}
+    </>
+  )
+})
+
+export default SubgraphLoader
