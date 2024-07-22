ba4e2b26dffb771535f290e531235e4829aa6a59
fernandomg <fernando.greco@gmail.com>
2024-07-22 17:03:51 +0200

    fix: set native token address to zero address by default

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,5 @@
 import { createEnv } from '@t3-oss/env-core'
+import { zeroAddress } from 'viem'
 import { z } from 'zod'
 
 export const env = createEnv({
@@ -15,12 +16,7 @@ export const env = createEnv({
       .optional()
       .default('true'),
     PUBLIC_INFURA_KEY: z.string().optional(),
-    PUBLIC_NATIVE_TOKEN_ADDRESS: z
-      .string()
-      .optional()
-      .default('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
-      // enforce lower case as 0xe address is widely used in that format
-      .transform((value) => value.toLowerCase()),
+    PUBLIC_NATIVE_TOKEN_ADDRESS: z.string().optional().default(zeroAddress),
     PUBLIC_RPC_ARBITRUM: z.string().optional(),
     PUBLIC_RPC_ARBITRUM_SEPOLIA: z.string().optional(),
     PUBLIC_RPC_BASE: z.string().optional(),
