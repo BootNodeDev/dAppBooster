5fdc9fa75b227cf549b15aaf773d3c67b4697819
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-07-24 15:42:48 -0300

    fix: update dropdown button to new db-ui-toolkit version

--- src/sharedComponents/DropdownButton.tsx
+++ src/sharedComponents/DropdownButton.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-import { ThemedButton } from 'db-ui-toolkit'
+import { Button } from 'db-ui-toolkit'
 
 const ChevronDown = () => (
   <svg
@@ -21,8 +21,8 @@ const ChevronDown = () => (
   </svg>
 )
 
-const DropdownButton = styled(ThemedButton).attrs(({ children }) => ({
-  $cssVarRoot: '--theme-dropdown-button',
+const DropdownButton = styled(Button).attrs(({ children }) => ({
+  $variant: 'dropdown',
   children: (
     <>
       {children} <ChevronDown />
