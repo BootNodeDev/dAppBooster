ba016eece1740211b88feb514b5e9be9483a3a54
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-07-25 16:59:29 -0300

    fix: test

--- src/pageComponents/home/Examples/demos/Avatar.test.tsx
+++ src/pageComponents/home/Examples/demos/Avatar.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import { Mock, describe, expect, it, vi } from 'vitest'
 import { useEnsName, useEnsAvatar } from 'wagmi'
 
-import Avatar from '@/src/pageComponents/home/Examples/demos/Avatar'
+import Avatar from '@/src/pageComponents/home/Examples/demos/AvatarDemo'
 
 // Mock wagmi hooks
 vi.mock('wagmi', () => ({
