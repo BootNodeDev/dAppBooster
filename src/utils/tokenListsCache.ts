1e9408d2910584963a8671385cbe1faaa1dcfe3b
fernandomg <fernando.greco@gmail.com>
2024-07-26 14:23:38 +0200

    docs: add descriptions to tokenListsCache

--- src/utils/tokenListsCache.ts
+++ src/utils/tokenListsCache.ts
@@ -1,5 +1,9 @@
 import type { Token, Tokens } from '@/src/types/token'
 
+/**
+ * Represents a cache for the list of tokens that is loaded at app startup.
+ * This file serves as a minimum version of the cache, potentially acting as a singleton.
+ */
 export type TokensMap = {
   tokens: Tokens
   tokensByChainId: { [chainId: Token['chainId']]: Tokens }
@@ -10,6 +14,10 @@ const tokenListsCache: TokensMap = {
   tokensByChainId: {},
 }
 
+/**
+ * Updates the token lists cache with the provided tokens map.
+ * @param tokensMap - The tokens map to update the cache with.
+ */
 export const updateTokenListsCache = (tokensMap: TokensMap) => {
   tokenListsCache.tokens = tokensMap.tokens
   tokenListsCache.tokensByChainId = tokensMap.tokensByChainId
