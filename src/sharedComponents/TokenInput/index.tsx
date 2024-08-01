6360139a7e69db4b824af42f0fee856e3c9c4cd7
fernandomg <fernando.greco@gmail.com>
2024-08-01 14:47:20 +0200

    fix: remove unused import

--- src/sharedComponents/TokenInput/index.tsx
+++ src/sharedComponents/TokenInput/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState, type FC } from 'react'
+import { useMemo, useState, type FC } from 'react'
 import styled from 'styled-components'
 
 import { useDialog, Spinner } from 'db-ui-toolkit'
