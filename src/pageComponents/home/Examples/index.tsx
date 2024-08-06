a280b55c8a99855dd794800ebe8321d818fda2c2
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-08-06 16:25:11 -0300

    chore: update Hash docs link

--- src/pageComponents/home/Examples/index.tsx
+++ src/pageComponents/home/Examples/index.tsx
@@ -99,7 +99,7 @@ const Examples: FC<HTMLAttributes<HTMLElement>> = ({ ...restProps }) => {
     },
     {
       demo: <HashDemo chain={mainnet} hash={address} />,
-      href: 'https://github.com/BootNodeDev/dAppBooster/blob/a7d0a994f6d5c2a547513669fccb619be046f1d2/src/sharedComponents/Hash.tsx#L32-L41',
+      href: 'https://bootnodedev.github.io/dAppBooster/modules/sharedComponents_Hash.html',
       icon: <ImgHash />,
       text: 'Copy, open in explorer',
       title: 'Hash component',
