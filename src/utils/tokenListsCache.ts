95b3c6bcb05aa78c468ff6ddccbbe93f662d35ea
nicosampler <nf.dominguez.87@gmail.com>
2024-08-12 17:32:13 -0300

    feat: add token manually to token list

--- src/utils/tokenListsCache.ts
+++ src/utils/tokenListsCache.ts
@@ -23,4 +23,25 @@ export const updateTokenListsCache = (tokensMap: TokensMap) => {
   tokenListsCache.tokensByChainId = tokensMap.tokensByChainId
 }
 
+/**
+ * Updates the token lists cache with the provided token.
+ * @param token - The token to add to the cache.
+ */
+export const addTokenToTokenList = (token: Token) => {
+  // if already exists, do not add
+  if (
+    tokenListsCache.tokens.some(
+      (t) => t.address.toLowerCase() === token.address.toLowerCase() && t.chainId === token.chainId,
+    )
+  ) {
+    return
+  }
+
+  tokenListsCache.tokens.push(token)
+  if (!tokenListsCache.tokensByChainId[token.chainId]) {
+    tokenListsCache.tokensByChainId[token.chainId] = []
+  }
+  tokenListsCache.tokensByChainId[token.chainId].push(token)
+}
+
 export default tokenListsCache
