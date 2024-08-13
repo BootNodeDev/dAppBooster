1e1558e7b7ac62d323b1ca0167df0d80cbc8f8a3
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-08-13 13:30:11 -0300

    refactor: simplify footer

--- src/components/sharedComponents/Footer.tsx
+++ src/components/sharedComponents/Footer.tsx
@@ -3,32 +3,14 @@ import styled from 'styled-components'
 
 import { InnerContainer, Footer as BaseFooter, LogoMini, ContainerPadding } from 'db-ui-toolkit'
 
-import Socials from '@/src/components/sharedComponents/Footer/Socials'
-
 export const Wrapper = styled(BaseFooter)`
-  color: var(--theme-footer-text-color);
   height: 92px;
 `
 
 const Inner = styled(InnerContainer)`
-  align-items: center;
-  column-gap: calc(var(--base-gap) + var(--base-gap) / 2);
   justify-content: center;
 
   ${ContainerPadding}
-`
-
-const Line = styled.div`
-  [data-theme='light'] & {
-    background-color: #c5c2cb;
-  }
-
-  [data-theme='dark'] & {
-    background-color: #5f6178;
-  }
-
-  height: 17px;
-  width: 1px;
 `
 
 export const Footer: FC = ({ ...restProps }) => {
@@ -43,8 +25,6 @@ export const Footer: FC = ({ ...restProps }) => {
         >
           <LogoMini />
         </a>
-        <Line />
-        <Socials />
       </Inner>
     </Wrapper>
   )
