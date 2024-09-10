0fed98d3a5d8c357863b2fec4dd83b5dc6c65ddd
Gabito Esmiapodo <gabriel@bootnode.dev>
2024-09-10 10:33:34 -0300

    docs: update footer

--- docs/footer.tsx
+++ docs/footer.tsx
@@ -1,8 +1,10 @@
+import React from 'react'
+
 export default function Footer() {
     return (
       <div>
         <div>Released under the MIT License.</div>
-        <div>Copyright © 2024-present - <a href="https://www.bootnode.dev/" target="_blank">BootNode</a></div>
+        <div style={{display: 'flex', alignItems: 'center', columnGap: '8px'}}>Copyright © 2024-present - <a href="https://www.bootnode.dev/" target="_blank"><img src="/img/footer/logo.svg" alt="BootNode" /></a></div>
       </div>
     )
   }
