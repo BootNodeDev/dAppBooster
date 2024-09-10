de1f95239ac816f4d301dc7b5d73c8923039c33b
fernandomg <fernando.greco@gmail.com>
2024-09-10 11:30:46 +0200

    fix: make wallet connect not-mandatory

--- src/env.ts
+++ src/env.ts
@@ -41,7 +41,7 @@ export const env = createEnv({
     PUBLIC_RPC_POLYGON: z.string().optional(),
     PUBLIC_RPC_POLYGON_MUMBAI: z.string().optional(),
     PUBLIC_RPC_SEPOLIA: z.string().optional(),
-    PUBLIC_WALLETCONNECT_PROJECT_ID: z.string().length(32),
+    PUBLIC_WALLETCONNECT_PROJECT_ID: z.string().optional().default(''),
     PUBLIC_INCLUDE_TESTNETS: zBoolean,
   },
   runtimeEnv: import.meta.env,
