40af733a0fd382f408c0657bae576dd15ce0c942
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-04-17 15:19:26 -0300

    fix: remove unnecessary props

--- src/components/ui/Inner.tsx
+++ src/components/ui/Inner.tsx
@@ -1,14 +1,11 @@
-import { Flex } from '@chakra-ui/react'
-import type { ComponentPropsWithoutRef, FC } from 'react'
+import { Flex, type FlexProps } from '@chakra-ui/react'
+import type { FC } from 'react'
 
-export const Inner: FC<ComponentPropsWithoutRef<'div'>> = ({ children, ...restProps }) => (
+export const Inner: FC<FlexProps> = ({ children, ...restProps }) => (
   <Flex
-    align="center"
-    justify="space-between"
     flexShrink={0}
-    h="100%"
-    mx="auto"
     maxW="100%"
+    mx="auto"
     px={{ base: 1, md: 2, xl: 4 }}
     w="var(--base-container-max-width)"
     {...restProps}
