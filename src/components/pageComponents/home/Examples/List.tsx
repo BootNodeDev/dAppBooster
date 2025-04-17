eabbb4837fbd42476744b550a5647f92c88ddd8f
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-04-17 18:31:38 -0300

    refactor(list): port to chakra ui

--- src/components/pageComponents/home/Examples/List.tsx
+++ src/components/pageComponents/home/Examples/List.tsx
@@ -1,60 +1,6 @@
-import type { FC, HTMLAttributes } from 'react'
-import styled, { css } from 'styled-components'
-
-import { breakpointMediaQuery } from '@bootnodedev/db-ui-toolkit'
-
 import Item, { type Props as ItemProps } from '@/src/components/pageComponents/home/Examples/Item'
-
-const Wrapper = styled.div`
-  [data-theme='light'] & {
-    --theme-examples-list-background-color: #e2e0e766;
-  }
-
-  [data-theme='dark'] & {
-    --theme-examples-list-background-color: #292b43;
-  }
-
-  align-items: center;
-  background-color: var(--theme-examples-list-background-color);
-  border-radius: var(--base-border-radius-xl);
-  display: flex;
-  flex-direction: column;
-  max-width: 100%;
-  padding: calc(var(--base-common-padding-xl) * 2) var(--base-common-padding);
-  row-gap: calc(var(--base-gap-xl) * 2);
-  width: 1066px;
-
-  ${breakpointMediaQuery(
-    'tabletLandscapeStart',
-    css`
-      padding: calc(var(--base-common-padding) * 7);
-      row-gap: calc(var(--base-gap) * 5);
-    `,
-  )}
-`
-
-const Title = styled.h2`
-  color: var(--theme-color-text-primary);
-  font-size: 2.1rem;
-  font-weight: 700;
-  line-height: 1.2;
-  margin: 0;
-
-  ${breakpointMediaQuery(
-    'tabletLandscapeStart',
-    css`
-      font-size: 3.6rem;
-    `,
-  )}
-`
-
-const Items = styled.div`
-  display: flex;
-  flex-direction: column;
-  max-width: 100%;
-  row-gap: calc(var(--base-gap) * 2);
-  width: 100%;
-`
+import { Flex, Heading } from '@chakra-ui/react'
+import type { FC, HTMLAttributes } from 'react'
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   items: ItemProps[]
@@ -62,17 +8,49 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 const List: FC<Props> = ({ items, ...restProps }) => {
   return (
-    <Wrapper {...restProps}>
-      <Title>Built-in Features</Title>
-      <Items>
+    <Flex
+      css={{
+        "[data-theme='light'] &": {
+          '--theme-examples-list-background-color': '#e2e0e766',
+        },
+        "[data-theme='dark'] &": {
+          '--theme-examples-list-background-color': '#292b43',
+        },
+      }}
+      align="center"
+      bg="var(--theme-examples-list-background-color)"
+      borderRadius="xl"
+      direction="column"
+      maxW="100%"
+      p={{ base: '32px 8px', lg: '56px' }}
+      rowGap={{ base: '32px', lg: '40px' }}
+      w="1066px"
+      {...restProps}
+    >
+      <Heading
+        color="var(--theme-color-text-primary)"
+        fontFamily="var(--base-font-family)"
+        fontSize={{ base: '21px', lg: '36px' }}
+        fontWeight="700"
+        lineHeight="1.2"
+        m="0"
+      >
+        Built-in Features
+      </Heading>
+      <Flex
+        direction="column"
+        maxW="100%"
+        rowGap={4}
+        w="100%"
+      >
         {items.map((item) => (
           <Item
             key={item.title}
             {...item}
           />
         ))}
-      </Items>
-    </Wrapper>
+      </Flex>
+    </Flex>
   )
 }
 
