558356a3a0d4ddadf441598b3de953b409e21094
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-04-25 11:53:55 -0300

    refactor(clouds): port to chakra ui

--- src/components/pageComponents/home/Welcome/Clouds/index.tsx
+++ src/components/pageComponents/home/Welcome/Clouds/index.tsx
@@ -1,49 +1,38 @@
-import styled, { css } from 'styled-components'
-
-import { breakpointMediaQuery } from '@bootnodedev/db-ui-toolkit'
-
 import ImgCloudsDark from '@/src/components/pageComponents/home/Welcome/Clouds/assets/img-clouds-dark.svg'
 import ImgCloudsLight from '@/src/components/pageComponents/home/Welcome/Clouds/assets/img-clouds-light.svg'
+import { Image, type ImageProps } from '@chakra-ui/react'
+import type { FC } from 'react'
 
-const Clouds = styled.img`
-  display: none;
-  flex-grow: 0;
-  flex-shrink: 0;
-  min-height: 306px;
-  min-width: calc(100vw + 400px);
-  object-fit: contain;
-  object-position: center bottom;
-
-  ${breakpointMediaQuery(
-    'desktopStart',
-    css`
-      min-width: fit-content;
-      object-fit: cover;
-      object-position: center;
-    `,
-  )}
-`
-
-Clouds.defaultProps = {
-  alt: '',
+const styles = {
+  display: 'none',
+  flexGrow: '0',
+  flexShrink: '0',
+  minHeight: '306px',
+  minWidth: { base: 'calc(100vw + 400px)', lg: 'fit-content' },
+  objectFit: { base: 'contain', lg: 'cover' },
+  objectPosition: { base: 'center bottom', lg: 'center' },
 }
 
-export const DarkClouds = styled(Clouds)`
-  [data-theme='dark'] & {
-    display: block;
-  }
-`
+export const DarkClouds: FC<ImageProps> = () => (
+  <Image
+    css={{
+      '[data-theme="dark"] &': {
+        display: 'block',
+      },
+    }}
+    src={ImgCloudsDark}
+    {...styles}
+  />
+)
 
-DarkClouds.defaultProps = {
-  src: ImgCloudsDark,
-}
-
-export const LightClouds = styled(Clouds)`
-  [data-theme='light'] & {
-    display: block;
-  }
-`
-
-LightClouds.defaultProps = {
-  src: ImgCloudsLight,
-}
+export const LightClouds: FC<ImageProps> = () => (
+  <Image
+    css={{
+      '[data-theme="light"] &': {
+        display: 'block',
+      },
+    }}
+    src={ImgCloudsLight}
+    {...styles}
+  />
+)
