fad6594a50abb67cda7773386dceaeeded2e48f7
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-04-25 15:06:08 -0300

    chore: update lint staged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,4 +1,4 @@
 export default {
-  // 'src/**/*': ['stylelint --fix', 'biome check --write', 'vitest related --run --coverage=false'],
-  'src/**/*': ['stylelint --fix', 'biome check --write'],
+  // 'src/**/*': ['biome check --write', 'vitest related --run --coverage=false'],
+  'src/**/*': ['biome check --write'],
 }
