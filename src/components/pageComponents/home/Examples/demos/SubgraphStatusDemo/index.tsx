7582d4a521d9ff7dcc366d57950f5a5555fa4733
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-04-25 10:47:58 -0300

    refactor(subgraph status demo): port to chakra ui

--- src/components/pageComponents/home/Examples/demos/SubgraphStatusDemo/index.tsx
+++ src/components/pageComponents/home/Examples/demos/SubgraphStatusDemo/index.tsx
@@ -4,118 +4,9 @@ import SkeletonLoading from '@/src/components/sharedComponents/ui/SkeletonLoadin
 import { env } from '@/src/env'
 import { withSuspenseAndRetry } from '@/src/utils/suspenseWrapper'
 import { type SchemaMappingConfig, useSubgraphIndexingStatus } from '@bootnodedev/db-subgraph'
-import { breakpointMediaQuery } from '@bootnodedev/db-ui-toolkit'
+import { Flex, Grid, Heading, Span } from '@chakra-ui/react'
 import { type FC, useState } from 'react'
-import styled, { css } from 'styled-components'
 import { type Chain, arbitrum, base, optimism, polygon } from 'viem/chains'
-
-const Wrapper = styled.div`
-  [data-theme='light'] & {
-    --theme-subgraph-status-background: #fff;
-    --theme-subgraph-status-data-row-color: #2e3048;
-    --theme-subgraph-status-data-color: #5f6178;
-  }
-
-  [data-theme='dark'] & {
-    --theme-subgraph-status-background: #373954;
-    --theme-subgraph-status-data-row-color: #fff;
-    --theme-subgraph-status-data-color: #e2e0e7;
-  }
-
-  display: flex;
-  flex-direction: column;
-  padding: var(--base-common-padding) 0 0;
-  row-gap: var(--base-gap-xl);
-  width: 100%;
-
-  ${breakpointMediaQuery(
-    'tabletPortraitStart',
-    css`
-      padding-top: calc(var(--base-common-padding) * 3);
-    `,
-  )}
-`
-
-const Row = styled.div`
-  background-color: var(--theme-subgraph-status-background);
-  border-radius: var(--base-border-radius);
-  display: flex;
-  flex-direction: column;
-  padding: var(--base-common-padding-xl);
-  row-gap: var(--base-gap-xl);
-  width: 100%;
-`
-
-const Title = styled.h3`
-  align-items: center;
-  color: var(--theme-subgraph-title-color);
-  column-gap: var(--base-gap);
-  display: flex;
-  font-size: 1.6rem;
-  font-weight: 700;
-  line-height: 1.2;
-  margin: 0;
-`
-
-const Data = styled.div<{ $status: 'error' | 'ok' }>`
-  --base-status-size: 10px;
-
-  align-items: center;
-  color: var(--theme-subgraph-status-data-row-color);
-  column-gap: var(--base-gap);
-  display: grid;
-  font-size: 1.6rem;
-  font-weight: 400;
-  grid-template-columns: var(--base-status-size) 1fr;
-  line-height: 1.2;
-  max-width: 100%;
-  white-space: nowrap;
-  width: fit-content;
-
-  &::before {
-    align-items: center;
-    background-color: var(
-      ${({ $status }) => ($status === 'error' ? '--theme-color-danger' : '--theme-color-ok')}
-    );
-    border-radius: 50%;
-    content: '';
-    display: flex;
-    height: var(--base-status-size);
-    width: var(--base-status-size);
-    transition: background-color var(--base-transition-duration);
-  }
-
-  ${breakpointMediaQuery(
-    'tabletPortraitStart',
-    css`
-      grid-template-columns: var(--base-status-size) auto 10px auto;
-    `,
-  )}
-`
-
-const SG = styled.span``
-
-const Separator = styled.span`
-  display: none;
-
-  ${breakpointMediaQuery(
-    'tabletPortraitStart',
-    css`
-      display: block;
-    `,
-  )}
-`
-
-const BC = styled.span`
-  padding-left: calc(var(--base-status-size) + var(--base-gap));
-
-  ${breakpointMediaQuery(
-    'tabletPortraitStart',
-    css`
-      padding-left: 0;
-    `,
-  )}
-`
 
 export const SkeletonLoadingItem = () => (
   <SkeletonLoading
@@ -145,22 +36,68 @@ const SubgraphStatus: FC<{
   const { chain, isSynced, networkBlockNumber, resource, subgraphBlockNumber } = indexingStatus
 
   return (
-    <Row>
-      <Title title={chain.name}>
+    <Flex
+      bgColor="var(--theme-subgraph-status-background)"
+      borderRadius="4px"
+      flexDirection="column"
+      padding={4}
+      rowGap={4}
+      width="100%"
+    >
+      <Heading
+        alignItems="center"
+        as="h3"
+        color="var(--theme-subgraph-title-color)"
+        columnGap={2}
+        display="flex"
+        fontSize="16px"
+        fontWeight="700"
+        lineHeight="1.2"
+        margin="0"
+        title={chain.name}
+      >
         {`${resource}@${chain.id}`}
         {getNetworkIcon(chain.name.toLowerCase())}
-      </Title>
-      <Data $status={isSynced ? 'ok' : 'error'}>
-        <SG>
+      </Heading>
+      <Grid
+        css={{
+          '--base-status-size': '10px',
+        }}
+        alignItems="center"
+        color="var(--theme-subgraph-status-data-row-color)"
+        columnGap={2}
+        display="grid"
+        fontSize="16px"
+        fontWeight="400"
+        gridTemplateColumns={{
+          base: 'var(--base-status-size) 1fr',
+          lg: 'var(--base-status-size) auto 10px auto',
+        }}
+        lineHeight="1.2"
+        maxWidth="100%"
+        whiteSpace="nowrap"
+        width="fit-content"
+        _before={{
+          alignItems: 'center',
+          backgroundColor: `${!isSynced ? 'var(--theme-color-danger)' : 'var(--theme-color-ok)'}`,
+          borderRadius: '50%',
+          content: "''",
+          display: 'flex',
+          height: 'var(--base-status-size)',
+          width: 'var(--base-status-size)',
+          transition: 'background-color var(--base-transition-duration)',
+        }}
+      >
+        <Span>
           <b>SG:</b> {subgraphBlockNumber.toString()}
-        </SG>
-        <Separator>-</Separator>
-        <BC>
+        </Span>
+        <Span display={{ base: 'none', lg: 'block' }}>-</Span>
+        <Span paddingLeft={{ base: 'calc(var(--base-status-size) + 8px)', lg: 0 }}>
           <b>BC:</b>
           {networkBlockNumber?.toString() ?? '-'}
-        </BC>
-      </Data>
-    </Row>
+        </Span>
+      </Grid>
+    </Flex>
   )
 }
 
@@ -208,7 +145,28 @@ const List = ({ ...restProps }) => {
   const items = chains.map((item) => makeItem(item))
 
   return (
-    <Wrapper {...restProps}>
+    <Flex
+      css={{
+        "[data-theme='light'] &": {
+          '--theme-subgraph-status-background': '#fff',
+          '--theme-subgraph-status-data-row-color': '#2e3048',
+          '--theme-subgraph-status-data-color': '#5f6178',
+        },
+        "[data-theme='dark'] &": {
+          '--theme-subgraph-status-background': '#373954',
+          '--theme-subgraph-status-data-row-color': '#fff',
+          '--theme-subgraph-status-data-color': '#e2e0e7',
+        },
+      }}
+      display="flex"
+      flexDirection="column"
+      paddingBottom={0}
+      paddingTop={{ base: 2, lg: 6 }}
+      paddingX={0}
+      rowGap="var(--base-gap-xl)"
+      width="100%"
+      {...restProps}
+    >
       <OptionsDropdown items={items} />
       {uniswapNetworks.map(
         (chain) =>
@@ -221,7 +179,7 @@ const List = ({ ...restProps }) => {
           ),
       )}
       {currentChain?.id === base.id && <Aave suspenseFallback={<SkeletonLoadingItem />} />}
-    </Wrapper>
+    </Flex>
   )
 }
 
