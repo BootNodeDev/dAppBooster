a4b47f7240ef1de0b9f4d4ce6b017bcb8bbdacd2
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-05-02 14:58:39 -0300

    refactor(list): remove code

--- src/components/pageComponents/home/Examples/List/index.tsx
+++ src/components/pageComponents/home/Examples/List/index.tsx
@@ -1,23 +1,18 @@
 import Item, { type Props as ItemProps } from '@/src/components/pageComponents/home/Examples/Item'
 import { Grid, type GridProps } from '@chakra-ui/react'
 import type { FC } from 'react'
-import styles from './styles'
 
 interface Props extends GridProps {
   items: ItemProps[]
 }
 
-const List: FC<Props> = ({ css, items, ...restProps }) => {
+const List: FC<Props> = ({ items, ...restProps }) => {
   return (
     <Grid
       minWidth="0"
       gap={6}
       templateColumns={{ base: '1fr', md: '1fr 1fr', lg: '1fr 1fr 1fr' }}
       w="100%"
-      css={{
-        ...styles,
-        ...css,
-      }}
       {...restProps}
     >
       {items.map((item) => (
