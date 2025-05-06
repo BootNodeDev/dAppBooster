54271897ceeccd1173aac9ed08c5930050af6409
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-05-05 22:44:47 -0300

    chore: add home and readme to .install-files

---
+++ .install-files/home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Home = () => (
+  <div>
+    Welcome to{' '}
+    <a
+      href="https://dappbooster.dev"
+      rel="noreferrer"
+      target="_blank"
+    >
+      dAppBooster
+    </a>
+    !
+  </div>
+)
+
+export default Home
