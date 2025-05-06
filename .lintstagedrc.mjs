508ae9941179064270bfdebdc756365f5679ab86
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-05-06 12:55:54 -0300

    fix: ignored biome files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,4 +1,4 @@
 export default {
   // 'src/**/*': ['biome check --write', 'vitest related --run --coverage=false'],
-  'src/**/*': ['biome check --write'],
+  'src/**/*.{js,jsx,ts,tsx,d.ts,json,jsonc}': ['biome check --write'],
 }
