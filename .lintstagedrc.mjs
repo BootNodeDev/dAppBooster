ad9ab8dfcae29a09bea4fc38fd52725c43e2032a
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-05-08 16:51:42 -0300

    fix(lint-staged): make the rules a bit more specific

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,8 @@
 export default {
-  'src/**/*': [
+  'src/**/*.{ts,tsx,js,jsx}': [
     'biome check --write',
     'vitest related --run --coverage=false',
     'tsc --noEmit --pretty --skipLibCheck --files',
   ],
+  'src/**/*.{json,jsonc,mjs,cjs}': ['biome check --write'],
 }
