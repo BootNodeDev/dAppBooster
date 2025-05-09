5edaf27527185cb7ff30c08a46604ba9afa67e82
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-05-09 11:37:36 -0300

    chore: update some packages and configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
-import path from 'node:path'
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { resolve } from 'node:path'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import Sitemap from 'vite-plugin-sitemap'
@@ -10,19 +10,18 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
-    TanStackRouterVite(),
     tsconfigPaths(),
     Sitemap({
       hostname: 'https://dappbooster.dev',
     }),
   ],
-
   envPrefix: 'PUBLIC_',
   resolve: {
     alias: {
-      '@/src': path.resolve(__dirname, './src'),
-      '@packageJSON': path.resolve(__dirname, 'package.json'),
+      '@/src': resolve(__dirname, './src'),
+      '@packageJSON': resolve(__dirname, 'package.json'),
     },
   },
   test: {
