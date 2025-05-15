5ec033ce4616cf8faf1d40feaa68fe83f5631044
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-05-15 13:00:04 -0300

    feat: make the footer prettier

--- docs/footer.tsx
+++ docs/footer.tsx
@@ -1,13 +1,16 @@
 export default function Footer() {
+  const date = new Date()
+
   return (
-    <div>
+    <div style={{ display: 'flex', justifyContent: 'space-between', fontSize: '13px' }}>
       <div>Released under the MIT License.</div>
       <div style={{ display: 'flex', alignItems: 'center', columnGap: '8px' }}>
-        Copyright © 2024-present -{' '}
+        © {date.getFullYear()} -{' '}
         <a
           href="https://www.bootnode.dev/"
           target="_blank"
           rel="noreferrer"
+          title={'BootNode - Web3, blockchain development'}
         >
           <img
             src="/img/footer/logo.svg"
