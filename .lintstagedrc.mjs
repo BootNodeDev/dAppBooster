9971ac8c5ec8b137fe8f74cf0be8681cd7f2e138
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2026-02-24 14:22:34 -0300

    chore: update lintstaged.mjs

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,4 +1,7 @@
 export default {
-  'src/**/*.{ts,tsx,js,jsx}': ['biome check --write', 'vitest related --run --coverage=false'],
-  'src/**/*.{json,jsonc,mjs,cjs}': ['biome check --write'],
+  'src/**/*.{ts,tsx,js,jsx}': [
+    'biome check --write --no-errors-on-unmatched',
+    'vitest related --run --coverage=false',
+  ],
+  'src/**/*.{json,jsonc,mjs,cjs}': ['biome check --write --no-errors-on-unmatched'],
 }
