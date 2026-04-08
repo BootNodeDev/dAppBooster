a49a4e12ecb115af668d8c05ea173805f0bc447b
fernandomg <fernando.greco@gmail.com>
2026-04-08 16:19:34 +0200

    fix: add BigInt.toJSON polyfill to prevent React 19 dev mode serialization crash

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
+// BigInt is not serializable by JSON.stringify. React 19 dev mode tries to serialize
+// component props for dev tools logging, which crashes when props contain BigInt values
+// (e.g., TransactionParams with value: parseEther('0.1')). This polyfill prevents the crash.
+;(BigInt.prototype as unknown as { toJSON: () => string }).toJSON = function () {
+  return this.toString()
+}
+
 import NotFound404 from '@/src/components/pageComponents/NotFound404'
 import { routeTree } from '@/src/routeTree.gen'
 import { printAppInfo } from '@/src/utils/printAppInfo'
