333beff418cefc2dbecc7766a1ace8089917bf3d
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2026-04-21 17:33:03 -0300

    fix(header): remove fixed height from logo to preserve aspect ratio

--- src/components/sharedComponents/ui/Header/Logo.tsx
+++ src/components/sharedComponents/ui/Header/Logo.tsx
@@ -15,7 +15,6 @@ const LogoLight =
 const Logo: FC<ImageProps> = ({ ...restProps }) => (
   <chakra.img
     width={193}
-    height={77}
     content="var(--base-logo)"
     display="block"
     flexShrink="0"
